96ed5a108649681c0ad676c1690fb07f7b9e104c
yoel <yoelsaf@gmail.com>
2022-10-26 01:02:43 +0300

    hi

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""add_item"""
+"""Script continually adds arguments to a JSON string in a file
+    called add_item.json
+"""
 
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-import json
-import sys
-import os.path
+if __name__ == "__main__":
+    try:
+        json_list = load_from_json_file("add_item.json")
 
-save_to_json = __import__('5-save_to_json_file').save_to_json_file
-load_from_json = __import__('6-load_from_json_file').load_from_json_file
+    except Exception:
+        json_list = []
 
-file = "add_item.json"
-if os.path.isfile(file):
-    Myobj = load_from_json(file)
-else:
-    Myobj = []
-Myobj.extend(sys.argv[1:])
-save_to_json(Myobj, file)
+    i = 1
+    while (i < len(argv)):
+        json_list.append(argv[i])
+        i += 1
+    save_to_json_file(json_list, "add_item.json")
